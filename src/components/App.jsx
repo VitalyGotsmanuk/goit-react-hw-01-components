0a0f15974d58b2af7d98259af3de6dede22fb340
VitalyGotsmanuk <v.gotsmanyuk@gmail.com>
2023-10-22 15:34:18 +0300

    0.2
    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import user from '../data/user.json';
 import { Profile } from './Profile';
+import data from '../data/data.json';
+import { Statistics } from './Statistics';
+
 
 
 export const App = () => {
   return (
     <section>
     <h1>Hi ALL!!!</h1>
-      
-    {/* <Product username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} /> */}
     
     <Profile
       username={user.username}
@@ -17,7 +18,10 @@ export const App = () => {
       stats={user.stats}
     />
         
-     
+    <Statistics title="Upload stats" stats={data} />
+      
+    <Statistics stats={data} />
+    
          
       
     </section> 
